c9a0eede8f03b5db42aacc68cb2821e3872ca0c9
xlpn0423 <xlpn0423@leeds.ac.uk>
2026-03-01 05:28:29 +0000

    2

--- worksheet/task2/task2.c
+++ worksheet/task2/task2.c
@@ -12,13 +12,47 @@
 int main(void){
 	long decimal=0;
 	char hex[9];
+	int len,i;
 	
 	printf("Enter a hexadecimal:");
 
+	fgets(hex,9,stdin);
+
+	len=strlen(hex);
+
+	if ( hex [len - 1] == '\n' ) {
+		
+		hex [len - 1] = '\0';
+		
+		len--;
+	}
+
+	for ( i = 0 ; i < len ; i++ ) {
+
+		char a = toupper(hex[i]);
+
+		if(a >= '0' && a <= '9'){
+
+			decimal = decimal + a - '0'; 
+		
+		} else if ( a >= 'A' && a <= 'F') {
+
+			decimal = decimal + a - 'A';
+
+		} else {
+
+			printf("Error: Invalid Hexadecimal\n");
+			
+			return 0;
+
+		}
+		
+	}
 	// if input contains invalid hex digit
 	// printf("Error: Invalid Hexadecimal\n");
 	
 	// print the decimal result
+
 	printf("decimal:%ld\n", decimal);
 	
 	return 0;
